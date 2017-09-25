654c69f4e9b7d6049a462cc09ef4860739511b74
Luke Halley <luke123halley@gmail.com>
2017-09-25 18:56:50 +0800

    Added Angular frontend

---
+++ public/javascripts/controllers/usercontroller.js
@@ -0,0 +1,7 @@
+var app = angular.module('InstrumApp');
+
+app.controller('userController', ['$scope', function($scope) {
+    // create a message to display in our view
+      $scope.message = 'This is the users routes!!';
+     }
+  ]);
