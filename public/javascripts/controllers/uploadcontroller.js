625d0e5d285864cc36bc459daa8b1dfd026e026e
Luke Halley <luke123halley@gmail.com>
2017-10-03 06:12:52 +0800

    Added pages and sidebar links

---
+++ public/javascripts/controllers/uploadcontroller.js
@@ -0,0 +1,7 @@
+var app = angular.module('InstrumApp');
+
+app.controller('uploadController', ['$scope', function($scope) {
+    // create a message to display in our view
+      $scope.message = 'This is the upload page!!';
+     }
+  ]);
