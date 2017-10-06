8168f9789fea3285b8f0ec80b784c8d589474d7e
Luke Halley <luke123halley@gmail.com>
2017-10-07 04:57:18 +0800

    User list working!!!
    :smiley: :smiley: :smiley:

--- public/javascripts/controllers/userscontroller.js
+++ public/javascripts/controllers/userscontroller.js
@@ -1,7 +1,20 @@
 var app = angular.module('InstrumApp');
 
-app.controller('usersController', ['$scope', function($scope) {
+app.controller('usersController', ['$scope', '$http' ,function($scope, $http) {
     // create a message to display in our view
-      $scope.message = 'This is the users page!!';
-     }
-  ]);
+    $scope.message = 'This is the users page!!';
+
+    findAll();
+
+    function findAll() {
+        $http.get('/users')
+            .success(function (data) {
+                $scope.users = data;
+                console.log(data);
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}]);
