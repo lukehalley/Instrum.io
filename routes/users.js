eeda92d92a716a507ab48ec7d79066f0e5c00a76
Luke Halley <luke123halley@gmail.com>
2017-10-19 21:57:34 +0800

    Todo

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ var db = mongoose.connection;
 // Connects to the MongoDB server
 // mongodb://<dbuser>:<dbpassword>@ds121494.mlab.com:21494/instrum-io
 // mongoose.connect('mongodb://localhost:27017/instrumdb');
+//TODO: Move password so its not public on GIT
 mongoose.connect('mongodb://lukehalley:0mkw4st5@ds121494.mlab.com:21494/instrum-io');
 
 // Catches an error if there is a problem connecting to the database, sends the message 'connection error' to the console along with the error (err)
